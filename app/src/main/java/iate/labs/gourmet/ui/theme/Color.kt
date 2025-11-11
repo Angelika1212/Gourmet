3c4abe379bbf772dc90746919758cead62582f4e
-show <zanzelika@yandex.ru>
2025-11-11 21:41:45 +0300

    style(ui): change color (beige), add image_icon for recipes and dimens

--- app/src/main/java/iate/labs/gourmet/ui/theme/Color.kt
+++ app/src/main/java/iate/labs/gourmet/ui/theme/Color.kt
@@ -3,5 +3,5 @@ package iate.labs.gourmet.ui.theme
 import androidx.compose.ui.graphics.Color
 
 val Red = Color(0xFFF21306)
-val Beige = Color(0xFFE9DBC5)
+val Beige = Color(0xFFF3D8C9)
 val RedGrey = Color(0xFF7C5C5C)
